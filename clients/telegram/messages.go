a2ed2f9e51bf806fe08cc4fa81dffea0ca50e0a2
Dmitriy Kononov <dimkkononov@gmail.com>
2024-06-11 19:09:18 +0300

    add constant messages for telegram client

---
+++ clients/telegram/messages.go
@@ -0,0 +1,34 @@
+package telegram
+
+const msgGreeting = `Привет, я бот для сохранения фильмов с Кинопоиска и не только
+
+Доступные команды:
+
+/random - посоветую случайный фильм
+/randomwithgosling - посоветую случайный фильм с Райаном Гослингом ;)
+/savemovie - сохранить фильм
+/removemovie - удалить фильм из сохраненного
+/mymovies - вывести все сохраненные фильмы`
+
+const msgInvalidSaveCommand = `Неверный формат команды!
+Отправьте ссылку в указанном формате:
+/savemovie https://www.kinopoisk.ru/film/0000/`
+
+const msgSave = `Для сохранения отправьте ссылку фильма на Кинопоиске после вызова команды.
+
+Пример: /savemovie https://www.kinopoisk.ru/film/0000/`
+
+const msgRemove = `Для удаления отправьте название фильма без кавычек, как оно записано в сохраненном.
+/mymovies - показать сохраненные фильмы
+
+Пример: /removemovie Драйв`
+
+const msgRemoveNotFound = `Фильм не найден в сохраненном. Проверьте название фильма.
+
+Пример: /removemovie Драйв`
+
+const kinopoiskMovieLink = "https://www.kinopoisk.ru/film/"
+
+const msgUnrecognized = "Неизвестная команда"
+
+const msgInDevelopment = "В разработке..."
